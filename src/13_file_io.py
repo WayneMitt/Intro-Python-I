a84ab739fd60df08ddd82f8378ae62998ffcb40e
Wayne Mitt <wmitt85@gmail.com>
2020-02-18 19:49:30 -0600

    completed 13_file_io exercise

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,12 +8,25 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+from pathlib import Path
 
-# YOUR CODE HERE
+file_to_open = Path(r'c:/Users/wayne/Documents/LambdaProjects/Sprint 11 - Intro to Python and OOP/Intro-Python-I/src/foo.txt')
+
+foo = open(file_to_open)
+
+print(foo.read())
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+bar = open("bar.txt", "w")
+bar.write(" Here's a line \n And another \n and a third")
+bar.close()
+
+file_to_open = Path(r'c:/Users/wayne/Documents/LambdaProjects/Sprint 11 - Intro to Python and OOP/Intro-Python-I/bar.txt')
+
+bar2 = open(file_to_open)
+print(bar2.read())
